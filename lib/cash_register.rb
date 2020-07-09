93575b2e3d78c04cab2336bb1a4811d52cf28935
Cameron Hudson <camnn3@yahoo.com>
2020-07-09 14:27:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,10 @@
-
+class CashRegister
+  attr_accessor :total, :apply_discount, :void_last_transaction, :items
+  
+  def initialize(apply_discount = 0)
+    @total = 0
+    @apply_discount = discount
+    @items = []
+  end
+  
+  def 
